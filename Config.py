ec776214f8d4b9b9779b2dd5bd477a3fc9dd28f4
PROFESSOR AGORA <98295372+ProfessorAgora@users.noreply.github.com>
2022-01-27 12:48:31 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -11,7 +11,7 @@ if ENVIRONMENT:
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
     DATABASE_URL = os.environ.get('DATABASE_URL', None)
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
-    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
+    
     MUST_JOIN = os.environ.get('MUST_JOIN', None)
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN.replace("@", "")
